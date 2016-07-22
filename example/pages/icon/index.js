1c8285fde88b6bb0ee1afdfff231bdae8ec5d982
ymma <328179934@qq.com>
2016-07-22 17:24:12 +0800

    upd icons

--- example/pages/icon/index.js
+++ example/pages/icon/index.js
@@ -15,13 +15,13 @@ export default class IconDemo extends React.Component {
                 <Icon value="fi-ok"></Icon>
 								<Icon value="fi-close"></Icon>
 								<Icon value="fi-attention"></Icon>
-								<Icon value="fi-dot"></Icon>
-								<Icon value="fi-down"></Icon>
+								<Icon value="fi-dot animate-spin"></Icon>
+								<Icon value="fi-down animate-spin"></Icon>
 								<Icon value="fi-left"></Icon>
 								<Icon value="fi-right"></Icon>
 								<Icon value="fi-up"></Icon>
-								<Icon value="fi-spin"></Icon>
-								<Icon value="fi-spin2"></Icon>
+								<Icon value="fi-spin animate-spin"></Icon>
+								<Icon value="fi-spin2 animate-spin"></Icon>
 								<Icon value="fi-home"></Icon>
 								<Icon value="fi-down-open"></Icon>
 								<Icon value="fi-up-open"></Icon>
