dcfc8682f5bed971b43107957f234ba6b5b83a66
ymma <328179934@qq.com>
2016-07-26 15:01:29 +0800

    lint fix

--- example/pages/icon/index.js
+++ example/pages/icon/index.js
@@ -13,35 +13,35 @@ export default class IconDemo extends React.Component {
             <Page className="icon-layout" title="Icon" spacing>
             	<Title text="All"></Title>
             	 	<Row className="row">
-                    <Col><Icon value="fi-ok"></Icon></Col>
-										<Col><Icon value="fi-close"></Icon></Col>
-										<Col><Icon value="fi-attention"></Icon></Col>
-										<Col><Icon value="fi-dot"></Icon></Col>
-                </Row>
-                <Row className="row">
+                        <Col><Icon value="fi-ok"></Icon></Col>
+    					<Col><Icon value="fi-close"></Icon></Col>
+    					<Col><Icon value="fi-attention"></Icon></Col>
+    					<Col><Icon value="fi-dot"></Icon></Col>
+                    </Row>
+                    <Row className="row">
                 		<Col><Icon value="fi-down"></Icon></Col>
                 		<Col><Icon value="fi-left"></Icon></Col>
-                    <Col><Icon value="fi-right"></Icon></Col>
-										<Col><Icon value="fi-up"></Icon></Col>
-                </Row>
-                <Row className="row">
+                        <Col><Icon value="fi-right"></Icon></Col>
+						<Col><Icon value="fi-up"></Icon></Col>
+                    </Row>
+                    <Row className="row">
                 		<Col><Icon value="fi-spin animate-spin"></Icon></Col>
-										<Col><Icon value="fi-spin2 animate-spin"></Icon></Col>
-										<Col><Icon value="fi-home"></Icon></Col>
-										<Col><Icon value="fi-down-open"></Icon></Col>
-                </Row>
-								<Row className="row">
+						<Col><Icon value="fi-spin2 animate-spin"></Icon></Col>
+						<Col><Icon value="fi-home"></Icon></Col>
+						<Col><Icon value="fi-down-open"></Icon></Col>
+                    </Row>
+					<Row className="row">
                 		<Col><Icon value="fi-up-open"></Icon></Col>
-										<Col><Icon value="fi-right-open"></Icon></Col>
-										<Col><Icon value="fi-left-open"></Icon></Col>
-										<Col><Icon value="fi-search"></Icon></Col>
-                </Row>
-								<Row className="row">
+						<Col><Icon value="fi-right-open"></Icon></Col>
+						<Col><Icon value="fi-left-open"></Icon></Col>
+						<Col><Icon value="fi-search"></Icon></Col>
+                    </Row>
+					<Row className="row">
                 		<Col><Icon value="fi-menu"></Icon></Col>
-										<Col></Col>
-										<Col></Col>
-										<Col></Col>
-                </Row>
+						<Col></Col>
+						<Col></Col>
+						<Col></Col>
+                    </Row>
             </Page>
         );
     }
