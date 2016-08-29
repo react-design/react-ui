504462c2c3914a2cf60188dceeb0fda75a7f4692
weinong <weinong@weinongdeMacBook-Pro.local>
2016-08-29 15:46:08 +0800

    upd document

--- doc/source/api/index.md
+++ doc/source/api/index.md
@@ -65,7 +65,9 @@ import 'dist/fruit-ui.css';
 
 ### 默认（default）
 <div class="comp-button" id="button_ex1"></div>
+
 *源码*
+
 ``` js
 <Button >default</Button>
 <Button type="info">info</Button>
@@ -74,10 +76,12 @@ import 'dist/fruit-ui.css';
 <Button type="danger">danger</Button>
 ```
 
-### 空
 
+### 空（plain）
 <div class="comp-button" id="button_ex2"></div>
+
 *源码*
+
 ``` js
 <Button plain>default</Button>
 <Button plain type="primary">primary</Button>]
@@ -85,7 +89,9 @@ import 'dist/fruit-ui.css';
 
 ### 图标
 <div class="comp-button" id="button_ex3"></div>
+
 *源码*
+
 ``` js
 <Button icon="fi-left-open f-left">default</Button>
 <Button icon="fi-right-open f-right">default</Button>
@@ -93,8 +99,10 @@ import 'dist/fruit-ui.css';
 <Button icon="fi-spin2 animate-spin" type="primary">primary</Button>
 ```
 
+
 ### 块
 <div class="comp-button" id="button_ex4"></div>
+
 *源码*
 
 ``` js
@@ -104,7 +112,9 @@ import 'dist/fruit-ui.css';
 
 ### 禁用
 <div class="comp-button" id="button_ex5"></div>
+
 *源码*
+
 ``` js
 <Button disabled="true">default</Button>
 <Button disabled type="primary">primary</Button>
@@ -112,7 +122,9 @@ import 'dist/fruit-ui.css';
 
 ### 小尺寸
 <div class="comp-button" id="button_ex6"></div>
+
 *源码*
+
 ``` js
 <Button small>default</Button>
 <Button small type="primary">primary</Button>
@@ -143,7 +155,9 @@ import 'dist/fruit-ui.css';
 ## Switch 开关
 
 ### 默认
+
 <div class="comp-switch" id="switch_ex1"></div>
+
 *源码*
 ``` html
 <Switch/>
@@ -151,13 +165,18 @@ import 'dist/fruit-ui.css';
 
 ### 小尺寸（small）
 <div class="comp-switch" id="switch_ex2"></div>
+
 *源码*
+
 ``` html
 <Switch size="small"/>
 ```
 ### 设置文字/图标
+
 <div class="comp-switch" id="switch_ex3"></div>
+
 *源码*
+
 ``` html
 <Switch size="normal" checkedChildren="on" uncheckedChildren="off"/>
 <Switch size="normal" defaultChecked="false" checkedChildren="开" uncheckedChildren="关"/>
@@ -165,21 +184,301 @@ import 'dist/fruit-ui.css';
 ```
 
 ### 禁用（disabled）
+
 <div class="comp-switch" id="switch_ex4"></div>
+
 *源码*
+
 ``` html
 <Switch size="normal" defaultChecked="false" disabled="true" checkedChildren="开" uncheckedChildren="关"/>
 <Switch size="small" defaultChecked="false" disabled="true"/>
 ```
 
 ### onChange事件
+
 <div class="comp-switch" id="switch_ex5"></div>
+
 *源码*
+
 ``` html
 <Switch size="normal" onChange={(e)=>this.handleChange(e)} defaultChecked="false"/>
 ```
 
+## Grid（格珊）
+
+### flex-等分
+
+<div class="comp-grid" id="grid_ex1"></div>
+
+*源码*
+``` html
+<Row>
+    <Col>1/1</Col>
+</Row>
+<Row>
+    <Col>1/2</Col>
+    <Col>1/2</Col>
+</Row>
+<Row>
+    <Col>1/3</Col>
+    <Col>1/3</Col>
+    <Col>1/3</Col>
+</Row>
+<Row>
+    <Col>1/4</Col>
+    <Col>1/4</Col>
+    <Col>1/4</Col>
+    <Col>1/4</Col>
+</Row>
+```
+
+### flex-自定义
+
+<div class="comp-grid" id="grid_ex2"></div>
+
+*源码*
+
+``` html
+<Row>
+    <Col flex="1">1/3</Col>
+    <Col flex="2">2/3</Col>
+</Row>
+<Row>
+    <Col flex="1">1/4</Col>
+    <Col flex="1">1/4</Col>
+    <Col flex="2">2/4</Col>
+</Row>
+```
+
+### 左固定-右适应
+
+<div class="comp-grid" id="grid_ex3"></div>
+
+*源码*
+
+``` html
+<Row>
+    <ColHead width="100">width:100px</ColHead>
+    <ColBody>auto</ColBody>
+</Row>
+<Row>
+    <ColHead width="200">width:200px</ColHead>
+    <ColBody>auto</ColBody>
+</Row>
+```
+
+### 左适应-右固定
+
+<div class="comp-grid" id="grid_ex4"></div>
+
+*源码*
+``` html
+<Row>
+    <ColBody>auto</ColBody>
+    <ColFoot width="100">width:100px</ColFoot>
+</Row>
+<Row>
+    <ColBody>auto</ColBody>
+    <ColFoot width="200">width:200px</ColFoot>
+</Row>
+```
 
 
+### 三列布局
+<div class="comp-grid" id="grid_ex5"></div>
+
+*源码*
+
+``` html
+<Row>
+    <ColHead width="100">width:100px</ColHead>
+    <ColBody>auto</ColBody>
+    <ColFoot width="100">width:100px</ColFoot>
+</Row>
+```
+
+## SearchBar（搜索框）
+
+### 默认
+
+<div class="comp-search" id="search_ex1"></div>
+
+*源码*
+
+``` html
+<SearchBar></SearchBar>
+```
+
+### 变化时的回调
+
+<div class="comp-search" id="search_ex2"></div>
+
+*源码*
+
+``` html
+<SearchBar placeholder="输入搜索内容" onChange={(value) => this.changeDemo(value)} button="false"></SearchBar>
+```
+
+### 搜索时的回调
+
+<div class="comp-search" id="search_ex3"></div>
+
+*源码*
+
+``` html
+<SearchBar placeholder="输入搜索内容" onSearch={(value) => this.searchDemo(value)}></SearchBar>
+```
+
+
+## Toast 提示
+
+<div class="comp-toast" id="toast_ex1"></div>
+
+*源码*
+
+``` js
+React.createClass({
+  getInitialState: function() {
+    return {show_top: false, show_normal:false}; 
+  },
+  topShowHandle: function() {
+    var self = this;
+    self.setState({show_top: true});
+    setTimeout(function(){self.setState({show_top: false}); },2000);
+  },
+  normalShowHandle: function() {
+    var self = this;
+    self.setState({show_normal: true});
+    setTimeout(function(){self.setState({show_normal: false}); },2000);
+  },
+  render: function() {
+      return (
+      <div>
+        <Button onClick={this.normalShowHandle}>正常提示</Button>
+        <Button type="primary" onClick={this.topShowHandle}>顶部提示</Button>
+        <Toast show={this.state.show_normal} icon="fi-spin animate-spin"> 正在加载中... </Toast>
+        <Toast show={this.state.show_top} icon="fi-attention" iconColor="#04be02" type="top" > 顶部成功 </Toast>
+      </div>
+    );
+  }
+});
+```
+
+## Message 提示
+
+<div class="comp-toast" id="message_ex"></div>
+
+*源码*
+
+``` js
+var loadingHide , loadingBlockHide ;
+React.createClass({
+  loadingHandle: function(show) {
+    if(show){
+      if(!loadingBlockHide){loadingBlockHide = Message.loading('正在加载中...'); }
+    }else{
+      loadingBlockHide();
+      loadingBlockHide = null;
+    }
+  },
+  loadingBlockHandle: function(show) {
+    if(show){
+      if(!loadingBlockHide){loadingBlockHide = Message.loading('正在加载中...',{type:'block'}); }
+    }else{
+      loadingBlockHide();
+      loadingBlockHide = null;
+    }
+  },
+  showHandle: function(arg) {
+    Message[arg.type](arg.text,{type : arg.showType});
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>方框提示</h3>
+        <Button onClick={this.showHandle.bind(this,{type:'info' ,showType:'block' ,text:'这是一条提示'})}>正常提示</Button>
+        <Button type="primary" onClick={this.loadingBlockHandle.bind(this,true)}>loading-show</Button>
+        <Button type="info" onClick={this.loadingBlockHandle.bind(this,false)}>loading-hide</Button>
+        <h3>文字提示</h3>
+        <Button type="primary" onClick={this.showHandle.bind(this,{type:'info',text:'这是一条提示'})}>正常提示</Button>
+        <Button type="warn" onClick={this.loadingHandle.bind(this, true)}>loading-show</Button>
+        <Button type="danger" onClick={this.loadingHandle.bind(this, false)}>loading-hide</Button>
+      </div>
+    );
+  }
+});
+```
+
+
+## Dialog（对话框）
+
+<div class="comp-toast" id="dialog_ex"></div>
+
+*源码*
+
+
+``` html
+React.createClass({
+  getInitialState: function() {return {show1 : false, show2 : false, show3 : false} }
+  dialogClickHandle : function(number){
+    var sets = {};
+    sets['show'+number] = !this.state['show'+number];
+    this.setState(sets);
+  },
+  dialogOnCancel : funciton(number){
+    var sets = {};
+    sets['show'+number] = false;
+    this.setState(sets);
+  },
+  render: function() {
+    var states = this.state;
+    var eventOnCancel = this.onCancel;
+    return (<div>
+      <Button type="info" onClick={this.dialogClickHandle.bind(this ,1)}>1个按钮</Button>
+      <Button type="warn" onClick={this.dialogClickHandle.bind(this ,2)}>3个按钮+自定义按钮颜色</Button>
+      <Button type="primary" onClick={this.dialogClickHandle.bind(this ,3)}>含标题</Button>
+      <Dialog show={states.show1} onOk={eventOnCancel.bind(this,1)} > 确认要提交吗 </Dialog>
+      <Dialog show={states.show2} footer={[{text:'取消',onClick:eventOnCancel.bind(this,2) ,color:'black'}, {text:'保存草稿',onClick:eventOnCancel.bind(this,2)}, {text:'提交',onClick:eventOnCancel.bind(this,2)}]} >
+        <p>我的测试</p>
+      </Dialog>
+      <Dialog show={states.show3} title="请求确认" onOk={eventOnCancel.bind(this,3)} > 确认要提交吗？ </Dialog>
+    </div>);
+  } });
+```
+
+## ActionSheet
+
+<div class="comp-actionSheet" id="actionSheet_ex"></div>
+
+*源码*
+
+``` js
+React.createClass({
+  getInitialState: function() {
+    return {show1 : false}
+  },
+  handleClick : function(){
+    this.state.show1 ? this.setState({show1:false}) : this.setState({show1:true})
+  },
+  onCancel : function(number){
+    this.setState({show1:false})
+  },
+  clickA : function(){
+    alert('您点击了第一个按钮');
+  },
+  render: function() {
+    return (<div>
+        <Button type="primary" onClick={this.handleClick} block>actionSheet</Button>
+        <ActionSheet show={this.state.show1} onRequestClose={this.onCancel}>
+          <ul>
+            <li onClick={this.clickA}>alert</li>
+            <li> <a href="tel:4008123123">订餐电话</a> </li>
+            <li onClick={this.onCancel}>取消</li>
+          </ul>
+        </ActionSheet>
+    </div> );
+  }
+});
+```
 
 
--- doc/themes/reactd/source/js/lc.js
+++ doc/themes/reactd/source/js/lc.js
@@ -8,7 +8,12 @@
 		ColHead = fui.ColHead ,
 		ColBody = fui.ColBody ,
 		ColFoot = fui.ColFoot ,
-		Switch 	= fui.Switch
+		Switch 	= fui.Switch ,
+		SearchBar = fui.SearchBar ,
+		Toast = fui.Toast ,
+		Message = fui.Message ,
+		Dialog = fui.Dialog ,
+		ActionSheet = fui.ActionSheet 
 
 
 	var reactResource = {
@@ -92,53 +97,90 @@
 			ReactDOM.render(
 				<div> 
 					<Row className="row">
-					    <Col className="color-1">1/1</Col>
-					</Row>
-					<Row className="row">
-					    <Col className="color-2">1/2</Col>
-					    <Col className="color-3">1/2</Col>
-					</Row>
+                    	<Col className="color-1">1/1</Col>
+	                </Row>
+	                <Row className="row">
+	                    <Col className="color-2">1/2</Col>
+	                    <Col className="color-3">1/2</Col>
+	                </Row>
+	                <Row className="row">
+	                    <Col className="color-4">1/3</Col>
+	                    <Col className="color-3">1/3</Col>
+	                    <Col className="color-5">1/3</Col>
+	                </Row>
+	                <Row className="row">
+	                    <Col className="color-4">1/4</Col>
+	                    <Col className="color-3">1/4</Col>
+	                    <Col className="color-1">1/4</Col>
+	                    <Col className="color-5">1/4</Col>
+	                </Row>
 				</div>, document.getElementById('grid_ex1'));
 
 			ReactDOM.render(
 				<div> 
 					<Row className="row">
-					    <Col className="color-1" flex="1">1/3</Col>
-					    <Col className="color-2" flex="2">2/3</Col>
-					</Row>
-					<Row className="row">
-					    <Col className="color-1" flex="1">1/4</Col>
-					    <Col className="color-3" flex="1">1/4</Col>
-					    <Col className="color-5" flex="2">2/4</Col>
-					</Row>
+	                    <Col className="color-1" flex="1">1/3</Col>
+	                    <Col className="color-2" flex="2">2/3</Col>
+	                </Row>
+	                <Row className="row">
+	                    <Col className="color-1" flex="1">1/4</Col>
+	                    <Col className="color-3" flex="1">1/4</Col>
+	                    <Col className="color-5" flex="2">2/4</Col>
+	                </Row>
 				</div>, document.getElementById('grid_ex2'));
 
 			ReactDOM.render(
 				<div> 
 					<Row className="row">
-					    <ColHead className="color-1" width="100">width:100px</ColHead>
-					    <ColBody className="color-2">auto</ColBody>
-					</Row>
+	                    <ColHead className="color-1" width="100">width:100px</ColHead>
+	                    <ColBody className="color-2">auto</ColBody>
+	                </Row>
+	                <Row className="row">
+	                    <ColHead className="color-3" width="200">width:200px</ColHead>
+	                    <ColBody className="color-4">auto</ColBody>
+	                </Row>
 				</div>, document.getElementById('grid_ex3'));
 
 			ReactDOM.render(
 				<div> 
 					<Row className="row">
-					    <ColBody className="color-5">auto</ColBody>
-					    <ColFoot className="color-3" width="100">width:100px</ColFoot>
-					</Row>
+	                    <ColBody className="color-5">auto</ColBody>
+	                    <ColFoot className="color-3" width="100">width:100px</ColFoot>
+	                </Row>
+	                <Row className="row">
+	                    <ColBody className="color-2">auto</ColBody>
+	                    <ColFoot className="color-4" width="200">width:200px</ColFoot>
+	                </Row>
 				</div>, document.getElementById('grid_ex4'));
 
 			ReactDOM.render(
 				<div> 
 					<Row className="row">
-					    <ColHead className="color-3" width="100">width:100px</ColHead>
-					    <ColBody className="color-2">auto</ColBody>
-					    <ColFoot className="color-4" width="100">width:100px</ColFoot>
-					</Row>
+	                    <ColHead className="color-3" width="100">width:100px</ColHead>
+	                    <ColBody className="color-2">auto</ColBody>
+	                    <ColFoot className="color-4" width="100">width:100px</ColFoot>
+	                </Row>
 				</div>, document.getElementById('grid_ex5'));
 		},
 		switch : function(){
+			var SwithEvent = React.createClass({
+				  getInitialState: function() {
+				    return {switched: 'false'};
+				  },
+				  handleClick: function(event) {
+				  	this.setState({switched: event.checked.toString()})
+				  },
+				  render: function() {
+				    return (
+				    	<div>
+					    	<Switch size="normal" onChange={this.handleClick} defaultChecked="false"/>
+			              	<div className="showCb">
+			                	当前状态是：{this.state.switched}
+			              	</div>
+		              	</div>
+				    );
+				  }
+				});
 			ReactDOM.render(
 				<div> 
 					<Switch/>
@@ -158,19 +200,225 @@
 
 			ReactDOM.render(
 				<div> 
-					<Switch/>
+					<Switch size="normal" defaultChecked="false" disabled="true" checkedChildren="开" uncheckedChildren="关"/>
+              		<Switch size="small" defaultChecked="false" disabled="true"/>
 				</div>, document.getElementById('switch_ex4'));
 
 			ReactDOM.render(
+				<SwithEvent />, document.getElementById('switch_ex5'));
+		},
+		search : function(){
+			var ChangeComponent = React.createClass({
+				getInitialState: function() {
+				    return {changeDemoValue: ''};
+				},
+				handleClick: function(value) {
+				  	this.setState({changeDemoValue: value})
+				},
+				render: function() {
+				    return (
+				    	<div>
+					    	<SearchBar placeholder="输入搜索内容" onChange={this.handleClick} button="false"></SearchBar>
+			              	<p className="showDemo">
+			                  搜索内容为：{this.state.changeDemoValue}
+			                </p>
+		              	</div>
+				    );
+				}
+			});
+			var CallbackComponent = React.createClass({
+				getInitialState: function() {
+				    return {searchDemoValue: ''};
+				},
+				handleClick: function(value) {
+				  	this.setState({searchDemoValue: value})
+				},
+				render: function() {
+				    return (
+				    	<div>
+					    	<SearchBar placeholder="输入搜索内容" onSearch={this.handleClick}></SearchBar>
+			              	<p className="showDemo">
+			                  搜索内容为：{this.state.searchDemoValue}
+			                </p>
+		              	</div>
+				    );
+				}
+			});
+			ReactDOM.render(
 				<div> 
-					<Switch/>
-				</div>, document.getElementById('switch_ex5'));
-		}
+					<SearchBar></SearchBar>
+				</div>, document.getElementById('search_ex1'));
+
+			ReactDOM.render(
+				<ChangeComponent />, document.getElementById('search_ex2'));
+
+			ReactDOM.render(
+				<CallbackComponent />, document.getElementById('search_ex3'));
+		},
+		toast : function(){
+			var ToastComponent = React.createClass({
+				getInitialState: function() {
+				    return {show_top: false, show_normal:false};
+				},
+				topShowHandle: function() {
+					var self = this;
+				  	self.setState({show_top: true});
+				  	setTimeout(function(){
+				  		self.setState({show_top: false});
+				  	},2000);
+				},
+				normalShowHandle: function() {
+					var self = this;
+				  	self.setState({show_normal: true});
+				  	setTimeout(function(){
+				  		self.setState({show_normal: false});
+				  	},2000);
+				},
+				render: function() {
+				    return (
+				    	<div>
+				    		<Button onClick={this.normalShowHandle}>正常提示</Button>
+					    	<Button type="primary" onClick={this.topShowHandle}>底部提示</Button>
+
+					    	<Toast show={this.state.show_normal} icon="fi-spin animate-spin"> 正在加载中... </Toast>
+			              	<Toast 
+				                show={this.state.show_top} 
+				                icon="fi-attention" 
+				                iconColor="#04be02"
+				                type="bottom"
+				              > 
+				              顶部成功 </Toast>
+		              	</div>
+				    );
+				}
+			});
+			ReactDOM.render(<ToastComponent />, document.getElementById('toast_ex1'));
+		},
+		message : function(){
+			var loadingHide , loadingBlockHide ;
+			var MessageComponent = React.createClass({
+				loadingHandle: function(show) {
+				  	if(show){
+				        if(!loadingBlockHide){
+				          loadingBlockHide = Message.loading('正在加载中...');
+				        }
+				    }else{
+				        loadingBlockHide();
+				        loadingBlockHide = null;
+				    }
+				},
+				loadingBlockHandle: function(show) {
+				  	if(show){
+				        if(!loadingBlockHide){
+				          loadingBlockHide = Message.loading('正在加载中...',{type:'block'});
+				        }
+				    }else{
+				        loadingBlockHide();
+				        loadingBlockHide = null;
+				    }
+				},
+				showHandle: function(arg) {
+					Message[arg.type](arg.text,{type : arg.showType});
+				},
+				render: function() {
+				    return (
+				    	<div>
+				    		<h3>方框提示</h3>
+				    		<Button onClick={this.showHandle.bind(this,{type:'info' ,showType:'block' ,text:'这是一条提示'})}>正常提示</Button>
+					    	<Button type="primary" onClick={this.loadingBlockHandle.bind(this,true)}>loading-show</Button>
+					    	<Button type="info" onClick={this.loadingBlockHandle.bind(this,false)}>loading-hide</Button>
+
+					    	<h3>文字提示</h3>
+					    	<Button type="primary" onClick={this.showHandle.bind(this,{type:'info',text:'这是一条提示'})}>正常提示</Button>
+					    	<Button type="warn" onClick={this.loadingHandle.bind(this, true)}>loading-show</Button>
+					    	<Button type="danger" onClick={this.loadingHandle.bind(this, false)}>loading-hide</Button>
+		              	</div>
+				    );
+				}
+			});
+			ReactDOM.render(<MessageComponent />, document.getElementById('message_ex'));
+		},
+		dialog : function(){
+			var DialogComponent = React.createClass({
+				getInitialState: function() {
+				    return {show1 : false, show2 : false, show3 : false}
+				},
+				dialogClickHandle : function(number){
+					var states = this.state;
+					var sets = {};
+		      		sets['show'+number] = !states['show'+number];
+		      		this.setState(sets);
+				},
+				dialogOnCancel : function(number){
+					var sets = {};
+			        sets['show'+number] = false;
+			        this.setState(sets);
+				},
+				render: function() {
+					var states = this.state;
+					var eventOnCancel = this.dialogOnCancel;
+				    return (
+				    	<div>
+				    		<Button type="info" onClick={this.dialogClickHandle.bind(this ,1)}>1个按钮</Button>
+				            <Button type="warn" onClick={this.dialogClickHandle.bind(this ,2)}>3个按钮+自定义按钮颜色</Button>
+				            <Button type="primary" onClick={this.dialogClickHandle.bind(this ,3)}>含标题</Button>
+				            <Dialog show={states.show1} onOk={eventOnCancel.bind(this,1)} > 确认要提交吗 </Dialog>
+			              <Dialog show={states.show2} footer={[
+			                  {text:'取消',onClick:eventOnCancel.bind(this,2) ,color:'black'},
+			                  {text:'保存草稿',onClick:eventOnCancel.bind(this,2)},
+			                  {text:'提交',onClick:eventOnCancel.bind(this,2)},
+			                ]} >
+			                <p>我的测试</p>
+			              </Dialog>
+			              <Dialog show={states.show3} title="请求确认" onOk={eventOnCancel.bind(this,3)} > 确认要提交吗？ </Dialog>
+		              	</div>
+				    );
+				}
+			});
+			ReactDOM.render(<DialogComponent />, document.getElementById('dialog_ex'));
+		},
+		actionSheet : function(){
+			var ActionSheetComponent = React.createClass({
+				getInitialState: function() {
+				    return {show1 : false}
+				},
+				handleClick : function(){
+					this.state.show1 ? this.setState({show1:false}) : this.setState({show1:true})
+				},
+				onCancel : function(number){
+					this.setState({show1:false})
+				},
+				clickA : function(){
+					alert('您点击了第一个按钮');
+				},
+				render: function() {
+				    return (
+				    	<div>
+				    		<Button type="primary" onClick={this.handleClick} block>actionSheet</Button>
+					        <ActionSheet show={this.state.show1} onRequestClose={this.onCancel}>
+					          <ul>
+					            <li onClick={this.clickA}>alert</li>
+					            <li> <a href="tel:4008123123">订餐电话</a> </li>
+					            <li onClick={this.onCancel}>取消</li>
+					          </ul>
+					        </ActionSheet>
+		              	</div>
+				    );
+				}
+			});
+			ReactDOM.render(<ActionSheetComponent />, document.getElementById('actionSheet_ex'));
+		},
 		
 	}
 	reactResource['button']();
 	reactResource['icon']();
 	reactResource['switch']();
+	reactResource['grid']();
+	reactResource['search']();
+	reactResource['toast']();
+	reactResource['message']();
+	reactResource['dialog']();
+	reactResource['actionSheet']();
 	console.log('load over');
 	return;
 	var _res = reactResource[type];
